c35f886a3fc42842441af7194e95e88bf7276237
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2022-07-07 17:30:51 +0300

    FE: Create section break component

---
+++ src/components/SectionBreak/SecitonBreak.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function SecitonBreak(): JSX.Element {
+  return (
+    <div className="max-w-[1240px] mx-auto mt-5 block w-32 h-1 rounded-full bg-gradient-to-r dark:from-light-accent dark:to-dark-accent from-dark-accent to-dark-primary" />
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from 'components/Hero';
 import { Layout } from 'layout/Layout';
+import SectionBreak from 'components/SectionBreak';
 
 export default function IndexPage(): JSX.Element {
   return (
     <Layout>
       <Hero />
+      <SectionBreak />
     </Layout>
   );
 }
