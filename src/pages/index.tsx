45e0aabf2c07d0fcb7da90db136bdfdcfe64980d
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2022-07-07 11:35:14 +0300

    Config: Remove dbConnect for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Hero from 'components/Hero';
 import { Layout } from 'layout/Layout';
-import dbConnect from 'lib/dbConnect';
 
 export default function IndexPage(): JSX.Element {
   return (
@@ -9,12 +8,4 @@ export default function IndexPage(): JSX.Element {
       <Hero />
     </Layout>
   );
-}
-
-export async function getServerSideProps() {
-  await dbConnect();
-
-  /* find all the data in our database */
-
-  return { props: { meow: 'hi' } };
 }
