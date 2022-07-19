59ee551625abd42aa518448500d293a2e497f61b
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2022-07-19 18:07:56 +0300

    Technologies: extracing data WIP

---
+++ src/components/Technologies/technologiesData.ts
@@ -0,0 +1,71 @@
+export const technologiesData = [
+  {
+    title: 'React',
+    description: 'Framework created by an evil corporation - ✔️',
+  },
+  {
+    title: 'Redux',
+    description: 'Get it from the store, use it anywhere',
+  },
+  {
+    title: 'TypeScript',
+    description: 'Types of types',
+  },
+  {
+    title: 'nodejs',
+    description: 'Running outside the browser',
+  },
+  {
+    title: 'javascript',
+    description: 'Obviously',
+  },
+];
+
+// <TechnologyCard
+// title="nodejs"
+// description="Running outside the browser"
+// />
+// <TechnologyCard title="javascript" description="Obviously" />
+// <TechnologyCard title="express" description="Who said async?" />
+// <TechnologyCard
+// title="mongodb"
+// description="The Snapchat of Databаses"
+// />
+// <TechnologyCard title="postgresql" description="A real Database" />
+// <TechnologyCard
+// title="GraphQL"
+// description="You only get what you ask for"
+// />
+// <TechnologyCard
+// title="firebase"
+// description="BaaS created by an evil corporation - ✔️"
+// />
+// <TechnologyCard title="Auth0" description="Authentication made easy" />
+// <TechnologyCard title="heroku" description="A cool guy" />
+// <TechnologyCard
+// title="next.js"
+// description="Because preparing things for prod is actually hard"
+// />
+// <TechnologyCard title="jest" description="Testing is fun" />
+// <TechnologyCard
+// title="Testing Library"
+// description="Testing is fun pt.2"
+// />
+// <TechnologyCard
+// title="CSS"
+// description="It's actually a fun thing to write"
+// />
+// <TechnologyCard title="HTML" description="Semantic tags" />
+// <TechnologyCard
+// title="Tailwind CSS"
+// description="Superhero utility library"
+// />
+// <TechnologyCard
+// title="Babel"
+// description="Because you kinda have to, am I  right?"
+// />
+// <TechnologyCard title="Webpack" description="For bundling" />
+// <TechnologyCard title="Git" description="Keeping track" />
+// <TechnologyCard title="Jira" description="For ticketing" />
+// <TechnologyCard title="Slack" description="Communication is key" />
+// <TechnologyCard title="Confluence" description="Documentation" />
