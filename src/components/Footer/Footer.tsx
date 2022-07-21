abc898da43ff82b402390bb0d112d33bd3a740b3
Tsvetislav Todorov <50547533+tsvetislavt99@users.noreply.github.com>
2022-07-21 10:56:44 +0300

    Create footer and fix nav logo link (#7)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,14 @@
+import SocialSection from 'components/SocialSection';
 import React from 'react';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="max-w-[1240px] mx-auto my-10 flex items-center justify-center">
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li className="-ml-5">
+          <SocialSection />
+        </li>
+      </ul>
+    </footer>
+  );
 }
