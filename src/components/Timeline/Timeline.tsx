529e861ae585fb367b666d76ea8640b666b6df89
Tsvetislav Todorov <50547533+tsvetislavt99@users.noreply.github.com>
2022-07-21 10:05:59 +0300

    Align timeline items | add margin to sections (#3)

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -4,8 +4,11 @@ import { timelineData } from './timelineData';
 
 export default function Timeline() {
   return (
-    <section>
-      <ol className="max-w-[1240px] mx-auto px-10 items-center sm:flex">
+    <section className="mx-auto max-w-[1240px]">
+      <h1 className="py-5 px-5 sm:px-10 text-2xl sm:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-dark-primary to-dark-primary/80 dark:from-light-primary dark:to-light-primary/70">
+        Timeline
+      </h1>
+      <ol className="max-w-[1240px] mx-auto px-10 items-start sm:flex mb-10">
         {timelineData.map((timeline) => (
           <TimelineCard key={timeline.title} {...timeline} />
         ))}
