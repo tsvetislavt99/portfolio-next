159519f3fbefb401eda2a0ab5688b17d10a3f48b
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2022-07-21 10:26:23 +0300

    Prevent link clicks from refreshing and add smooth scrolling behaviour

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from '../ProjectCard';
 
 export default function Projects() {
   return (
-    <div className="mx-auto max-w-[1240px]">
+    <div id="projects" className="mx-auto max-w-[1240px]">
       <h1 className="py-5 px-5 sm:px-10 text-2xl sm:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-dark-primary to-dark-primary/80 dark:from-light-primary dark:to-light-primary/70">
         Projects
       </h1>
