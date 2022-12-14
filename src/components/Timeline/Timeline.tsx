2c2f2293c281681c176488c578460a982d471432
Tsvetislav <tsvetislav.todorov@mentormate.com>
2022-12-14 10:47:21 +0200

    Change timeline layout on mobile

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -8,7 +8,7 @@ export default function Timeline() {
       <h1 className="py-5 px-5 sm:px-10 text-xl sm:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-dark-primary to-dark-primary/80 dark:from-light-primary dark:to-light-primary/70">
         Timeline
       </h1>
-      <ol className="max-w-[1240px] mx-auto px-10 items-start sm:flex mb-10">
+      <ol className="max-w-[1240px] mx-auto px-10 items-start lg:flex mb-10">
         {timelineData.map((timeline) => (
           <TimelineCard key={timeline.title} {...timeline} />
         ))}
