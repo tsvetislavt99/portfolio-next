3163f57e6d831751f5c62d6ac01384972ec7f166
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2022-12-20 14:33:35 +0200

    Add layout props

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 
-export const Layout = ({ children }) => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
   return (
     <div>
       <Header />
