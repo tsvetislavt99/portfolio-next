b4fcccd88e0f04df7d741d5e85c70b7cdf29e403
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2022-12-20 23:23:09 +0200

    Add tilt shake on hover

--- src/components/Hero/HeroHeading/HeroHeading.tsx
+++ src/components/Hero/HeroHeading/HeroHeading.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 import { onMouseLeaveHandler, onMouseMoveHandler } from 'utils/text-wave';
 
 const NAME = 'Tsvetislav Todorov';
@@ -6,7 +8,7 @@ const NAME = 'Tsvetislav Todorov';
 export default function HeroHeading() {
   return (
     <h1
-      onMouseMove={onMouseMoveHandler}
+      onMouseMove={isMobile ? () => {} : onMouseMoveHandler}
       className="pb-5 text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-dark-primary to-dark-primary/80 dark:from-light-primary dark:to-light-primary/70"
     >
       {NAME.split('').map((letter, index) =>
@@ -16,7 +18,7 @@ export default function HeroHeading() {
           <span
             key={index}
             className="inline-block"
-            onMouseLeave={onMouseLeaveHandler}
+            onMouseLeave={isMobile ? () => {} : onMouseLeaveHandler}
           >
             {letter}
           </span>
