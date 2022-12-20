561b2452294b1a0a5334faaaa73d1ca3d986d358
Tsvetislav Todorov <tsvetislavt99@gmail.com>
2022-12-20 14:25:17 +0200

    Rebase

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,34 @@
+import BlogCard from 'components/BlogCard';
 import React from 'react';
 
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <section className="bg-primary dark:bg-primary">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="grid gap-8 lg:grid-cols-2">
+          <BlogCard
+            type="Article"
+            title="How to use Next.js with TypeScript"
+            summary="Next.js is a React framework that allows you to build server-side rendered applications. It is a great tool for building static websites and web applications. It is also a great tool for building static websites and web applications."
+            updatedDate={new Date('2022-08-01')}
+            href="/blog/how-to-use-nextjs-with-typescript"
+          />
+          <BlogCard
+            type="Article"
+            title="How to use Next.js with TypeScript"
+            summary="Next.js is a React framework that allows you to build server-side rendered applications. It is a great tool for building static websites and web applications. It is also a great tool for building static websites and web applications."
+            updatedDate={new Date('2021-08-01')}
+            href="/blog/how-to-use-nextjs-with-typescript-2"
+          />
+          <BlogCard
+            type="Article"
+            title="How to use Next.js with TypeScript"
+            summary="Next.js is a React framework that allows you to build server-side rendered applications. It is a great tool for building static websites and web applications. It is also a great tool for building static websites and web applications. It is also a great tool for building static websites and web applications. It is also a great tool for building static websites and web applications."
+            updatedDate={new Date('2021-08-01')}
+            href="/blog/how-to-use-nextjs-with-typescript-3"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
---
+++ src/utils/format-summary.ts
@@ -0,0 +1,17 @@
+const SUMMARY_MAX_LENGTH = 257;
+/**
+ * @function formatSummary
+ * @param {string} summary The summary of the article
+ * @returns {string} The formatted summary
+ * @description Format the summary of the article. It should be a string with a maximum of 207 characters.
+ * If the summary is longer than 207 characters, it will be truncated and an ellipsis will be added. The function will cut the string at the last space before the 207th character.
+ */
+export const formatSummary = (summary: string): string => {
+  if (summary.length > SUMMARY_MAX_LENGTH) {
+    return `${summary.substring(
+      0,
+      summary.lastIndexOf(' ', SUMMARY_MAX_LENGTH),
+    )}...`;
+  }
+  return summary;
+};
