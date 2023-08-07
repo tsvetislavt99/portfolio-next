c2403ff5a23ac9b882bd2a913fa8f32baf602d24
Tsvetislav Todorov <t.todorov@draftkings.com>
2023-08-07 16:52:55 +0300

    Add some SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,19 @@ import Technologies from 'components/Technologies';
 import Timeline from 'components/Timeline';
 import AboutMe from 'components/AboutMe';
 import Recommendations from 'components/Recommendations';
+import { NextSeo } from 'next-seo';
 
 export default function IndexPage(): JSX.Element {
   return (
+    <>
+    <NextSeo
+      title="T.Works"
+      description="Tsvetislav's personal website."
+      openGraph={{
+       type: 'website',
+       siteName: 'Tsvetislav Todorov Personal Website',
+      }}
+    />
     <Layout>
       <Hero />
       <SectionBreak />
@@ -22,5 +32,6 @@ export default function IndexPage(): JSX.Element {
       <Technologies />
       <SectionBreak />
     </Layout>
+    </>
   );
 }
